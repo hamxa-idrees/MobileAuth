82f04724b59d287e00490c22e3f56abdea6a0342
Hamxa-idrees <hamza.idrees743@gmail.com>
2018-03-09 02:55:22 +0500

    test

---
+++ app/src/main/java/com/example/bnc/mobileauth/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bnc.mobileauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
